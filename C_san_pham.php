be3cdf0d388ec322f0ed327945b0da367a6b5b9a
tienthuan1412 <tienthuan1412@gmail.com>
2018-01-02 14:24:13 +0700

    add function DSSKM and DS Hang Xe in C_san_pham

--- C_san_pham.php
+++ C_san_pham.php
@@ -8,7 +8,25 @@ class C_san_pham
 		$smarty=new Smarty_ung_dung();
 		$smarty->display('index.tpl');
 	}
-	
+	public function DSSanPhamKhuyenMai()
+	{
+		$m_san_pham=new M_san_pham();
+		$DSSanPham=$m_san_pham->getDSSanPhamGiamGia();
+		
+		$smarty=new Smarty_ung_dung();
+		$smarty->assign('DSSanPham',$DSSanPham);
+		$smarty->assign('LoaiCha', array('ten_loai'=>'Danh sách sản phẩm khuyến mãi'));
+		$smarty->display('san_pham/v_doc_ds_san_pham_theo_loai.tpl');
+	}
+
+	public function DSHangXe()
+	{
+		$smarty=new Smarty_ung_dung();
+		$m_san_pham=new M_san_pham();
+		$DSHangXe=$m_san_pham->getDSHangXe();
+		$smarty->assign('DSHangXe',$DSHangXe);
+		$smarty->display('san_pham/v_doc_ds_hang_xe.tpl');
+	}
 
 }
 ?>
