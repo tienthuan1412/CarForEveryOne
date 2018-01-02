9dde4bed79c6e286dce014fcd9fba15c3d40fdd3
Huucuong96 <32858472+Huucuong96@users.noreply.github.com>
2018-01-02 14:44:50 +0700

    add function them mat hang vao gio hang, them vao gio hang, huy gio hang, thong tin gio hang in C_khach_hang

--- controllers/C_khach_hang.php
+++ controllers/C_khach_hang.php
@@ -25,5 +25,100 @@ class C_khach_hang
 		);	
 	}
 
-	
+	public function ThemMatHangVaoGioHang()
+	{
+		if(isset($_POST['sanpham']))
+		{
+			$masanpham=$_POST['sanpham'];
+			$soluong=1;
+			$m_san_pham=new M_san_pham();
+			$sanpham=$m_san_pham->getSanPham($masanpham);
+			if($sanpham)
+			{
+				$gio_hang=new Gio_hang();
+				if($sanpham['gia_khuyen_mai']>0)
+					$gio_hang->Them($masanpham,$sanpham['ten_xe'],$sanpham['gia_khuyen_mai'],$soluong,$sanpham['hinh']);
+				else
+					$gio_hang->Them($masanpham,$sanpham['ten_xe'],$sanpham['don_gia'],$soluong,$sanpham['hinh']);
+				echo number_format($gio_hang->TongSoLuong()) .' SP ('. number_format($gio_hang->TongSoTien()) . ' vnđ)';
+			}
+			else
+			{
+				echo '';
+			}
+		}
+		else
+		{
+			echo '';
+		}
+		
+	}
+	public function ThemVaoGioHang()
+	{
+		if(isset($_POST['mbst']) && $_POST['so_luong'])
+		{
+			$mbts=$_POST['mbst'];
+			$soluong=$_POST['so_luong'];
+			$m_bo_suu_tap=new m_bo_suu_tap();
+			$bosuutap=$m_bo_suu_tap->getBoSuuTap($mbst);
+			if($bosuutap)
+			{
+				$gio_hang=new Gio_hang();
+				if($bosuutap['gia_khuyen_mai']>0)
+					$gio_hang->Them($mbst.'-bst',$bosuutap['ten_bo_suu_tap'],$bosuutap['gia_khuyen_mai'],$soluong,$bosuutap['hinh']);
+				else
+					$gio_hang->Them($mbst.'-td',$bosuutap['ten_bo_suu_tap'],$bosuutap['don_gia'],$soluong,$bosuutap['hinh']);
+				echo number_format($gio_hang->TongSoLuong()) .' SP ('. number_format($gio_hang->TongSoTien()) . ' vnđ)';
+			}
+			else
+			{
+				echo '';
+			}
+		}
+		else
+		{
+			echo '';
+		}
+	}
+	public function ThongTinGioHang()
+	{
+		$gio_hang=new Gio_hang();
+		if(isset($_POST['CapNhat']))
+		{
+			$TTGioHang=$gio_hang->ThongTinGioHang();
+			foreach ($TTGioHang as $masanpham => $TT) {
+				$SlMoi=$_POST['sl_'.$masanpham];
+				if($SlMoi!=$TT[1])
+					$gio_hang->CapNhatGioHang($masanpham,$SlMoi);
+			}
+		}
+
+		$TTGioHang=$gio_hang->ThongTinGioHang();
+		
+		$smarty=new Smarty_ung_dung();
+
+		if($TTGioHang)
+		{
+			$mangSanPham=array();
+			$mangBoSuuTap=array();
+			foreach ($TTGioHang as $ma => $TT) {
+				if(strpos($ma,'-bst')!==false)
+					$mangBoSuuTap[$ma]=$TT;
+				else
+					$mangSanPham[$ma]=$TT;
+			}
+			if(count($mangSanPham)>0)
+				$smarty->assign('mangSanPham',$mangSanPham);
+				
+			if(count($mangBoSuuTap)>0)
+				$smarty->assign('mangBoSuuTap',$mangBoSuuTap);
+		}
+		$smarty->display('khach_hang/v_thong_tin_gio_hang.tpl');
+	}
+	public function HuyGioHang()
+	{
+		$gio_hang=new Gio_hang();
+		$gio_hang->HuyGioHang();
+		header('location:'.path.'khach-hang/thong-tin-gio-hang.html');
+	}
 }
