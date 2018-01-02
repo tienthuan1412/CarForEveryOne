8e09c9a973f8b5467732ac9200128fa3784784f0
mrusuno <32637053+mrusuno@users.noreply.github.com>
2018-01-02 14:35:00 +0700

    Add function LienHe va DichVu in C_DichVu

--- C_dich_vu.php
+++ C_dich_vu.php
@@ -10,5 +10,45 @@ class C_dich_vu
 	}
 	
 	
-	
+	public function DichVu()
+	{
+		$smarty=new Smarty_ung_dung();
+		$smarty->display('dich_vu/v_dich_vu.tpl');
+	}
+
+	public function LienHe()
+	{
+		$smarty=new Smarty_ung_dung();
+		$dataLienHe=array(
+			'ten'=>'', 
+			'email'=>'', 
+			'tieu_de'=>'', 
+			'noi_dung'=>''
+		);
+		$dataErr=array('ten'=>'', 'email'=>'', 'tieu_de'=>'', 'noi_dung'=>'');
+		if(isset($_POST['gui']))
+		{
+			$dataLienHe=array(
+				'ten'=>$_POST['ten'],
+				'email'=>$_POST['email'], 
+				'tieu_de'=>$_POST['tieu_de'], 
+				'noi_dung'=>$_POST['noi_dung']
+				);
+			$res=1;
+			$dataErr=$this->KiemTraDuLieu($dataLienHe,$res);
+			if($res==1)
+			{
+				$m_lien_he = new M_lien_he();
+				$res=$m_lien_he->ThemLienHe($dataLienHe);
+				if($res)
+					header('location:'.path.'lien-he.html');
+				else
+					$dataErr['mss']='Thêm không thành công';
+			}
+		}
+		$m_lien_he=new M_lien_he();
+		$smarty->assign('data',$dataLienHe);
+		$smarty->assign('dataErr',$dataErr);
+		$smarty->display('dich_vu/v_lien_he.tpl');
+	}
 }
